8c1fd5c3136abfcc038cd9c2124ca3b04c1427f7
Harsh Kansal <harshkansal303@gmail.com>
2024-10-27 03:19:42 +0000

    container commit done

---
+++ javascript/new.js
@@ -0,0 +1,13 @@
+const userId =1234
+
+let userEmail = "abc@gmail.com"
+var userPassword = "user@1234"
+
+userCity = "jaipur"
+
+
+userEmail = "xyz@gmail.com"
+userPassword="abcd"
+
+console.log(userEmail);
+console.table([userEmail,userId,userPassword,userCity])
