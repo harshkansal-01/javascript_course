acb5ea67dd69d849aff7deef70f9d354bf0818c9
Harsh Kansal <harshkansal303@gmail.com>
2024-10-28 06:43:19 +0000

    comparsions edit

--- javascript/comparisons.js
+++ javascript/comparisons.js
@@ -1,14 +1,15 @@
-// console.log(2>1);
-// console.log(2>=1);
+console.log(2>1);
+console.log(2>=1);
 
-// console.log(2<1);
+console.log(2<1);
 
-// console.log(2==1);
-// console.log(2!=1);
+console.log(2==1);
+console.log(2!=1);
 
-// console.log(undefined ==0)
-// console.log(undefined >0)
-// console.log(undefined>=0)
+console.log(undefined ==0)
+console.log(undefined >0)
+console.log(undefined>=0)
 
 
 console.log("2"==="2");
+
