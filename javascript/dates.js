69c23d0ec1571da6b9f21cadb3459e2b8a4e0fb8
Harsh Kansal <harshkansal303@gmail.com>
2024-11-01 17:48:38 +0000

    dates

---
+++ javascript/dates.js
@@ -0,0 +1,26 @@
+    let myDate = new Date();
+    const rect = new String("harsh")
+    // console.log(myDate.toLocaleDateString());
+
+    // console.log(typeof myDate);
+
+    let createdDate =  new Date(2023,24,11)
+    // console.log(createdDate.toString());
+
+
+    let newDate = new Date();
+    
+    console.log(newDate.toLocaleString('default',{
+        weekday:"long"
+    }));
+    
+
+
+//     let newDate = new Date();
+
+// Use backticks for string interpolation
+// console.log(`The day is ${newDate.getDay()}`);
+    
+ 
+
+
