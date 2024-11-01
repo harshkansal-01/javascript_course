4bfe3866bef63e8792c55c77239e8ce095e164eb
Harsh Kansal <harshkansal303@gmail.com>
2024-11-01 10:24:47 +0000

    strings

--- javascript/strings.js
+++ javascript/strings.js
@@ -1,10 +1,11 @@
 const gameName = new String("ha rsh   kan sal")
 
 
-// console.log(gameName[0]);
-// console.log(gameName.charAt(0));
-// console.log(gameName.indexOf('h'));
-// console.log(gameName.slice(-8,5));
-// console.log(gameName.split(' '));
+
+console.log(gameName[0]);
+console.log(gameName.charAt(0));
+console.log(gameName.indexOf('h'));
+console.log(gameName.slice(-8,5));
+console.log(gameName.split(' '));
 console.log(gameName.includes('ha  '));
  
